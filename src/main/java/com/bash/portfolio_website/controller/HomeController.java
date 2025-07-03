8088c44e264ae28472c31faab62456f8f0d6da79
Bashir Muhammed <bashmufol@gmail.com>
2025-07-03 20:37:32 +0100

    Updated

--- src/main/java/com/bash/portfolio_website/controller/HomeController.java
+++ src/main/java/com/bash/portfolio_website/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+//  Home URL
     @GetMapping("/")
     public String showHomePage(Model model) {
         model.addAttribute("title", "Home");
